d96fe69dd6eb91747527791076121ce7abad73c9
Joshua Kaminetsky <ethersage@users.noreply.github.com>
2020-06-16 15:09:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-console.log('kamisay');
+var quantile = require("@ethersage/quantile").quantile;
+
+console.log("kamisay");
+console.log(quantile(2, [5, 6, 7, 8, 1, 2, 3, 4])));
+console.log("kamisaid");
