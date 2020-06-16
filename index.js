06aa2c1868866c36be88ecedf66ff12b02df1f4b
Joshua Kaminetsky <ethersage@users.noreply.github.com>
2020-06-16 15:17:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 var quantile = require("@ethersage/quantile").quantile;
 
 console.log("kamisay");
-console.log(quantile(2, [5, 6, 7, 8, 1, 2, 3, 4])));
+console.log(quantile(2, [5, 6, 7, 8, 1, 2, 3, 4]));
 console.log("kamisaid");
