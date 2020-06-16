a33e68caedd9999eeddd9cde178f9f6e6656500e
Joshua Kaminetsky <ethersage@users.noreply.github.com>
2020-06-16 15:01:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const bucket = require('@ethersage/quantile').bucket;
-const quantile = require('@ethersage/quantile').quantile;
+var bucket = require('@ethersage/quantile').bucket;
+var quantile = require('@ethersage/quantile').quantile;
 
 console.log(
   bucket(2, [5, 6, 7, 8, 1, 2, 3, 4])); // => [[1, 2, 3, 4], [5, 6, 7, 8]],
