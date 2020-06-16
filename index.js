3e26974a7a542fa14be384dea3610fc4e387ec8d
Joshua Kaminetsky <ethersage@users.noreply.github.com>
2020-06-16 15:05:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-/*
-var bucket = require('@ethersage/quantile').bucket;
-var quantile = require('@ethersage/quantile').quantile;
-
-console.log(
-  bucket(2, [5, 6, 7, 8, 1, 2, 3, 4])); // => [[1, 2, 3, 4], [5, 6, 7, 8]],
-  bucket(3, [5, 6, 7, 8, 1, 2, 3, 4])); // => [[1, 2, 3], [4, 5, 6], [7, 8]],
-  bucket(3, [5, 6, 5, 3, 1, 2, 3, 4])); // => [[1, 2, 3], [3, 4, 5], [5, 6]],
-
-  quantile(2, [5, 6, 7, 8, 1, 2, 3, 4])); // => [4],
-  quantile(3, [5, 6, 7, 8, 1, 2, 3, 4])); // => [3, 6],
-  quantile(3, [5, 6, 5, 3, 1, 2, 3, 4])); // => [3, 5],
-);
-*/
-
 console.log('test');
